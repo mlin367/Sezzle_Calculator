8b98d5ca1765d329d63012257d385aba25b8fa15
Matthew Lin <mlin367@yahoo.com>
2019-06-03 21:07:12 -0700

    implemented onclick for the numbers

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,16 +7,30 @@ const socket = io('http://localhost:5000');
 
 class App extends React.Component {
   state = {
+    expression: ''
+  };
 
+  handleOnClick(e) {
+    const value = e.target.getAttribute('data-value');
+    if (value === 'C') this.setState({ expression: '' });
+    else if (value === 'CE')
+      this.setState({
+        expression: this.state.expression.slice(0, this.state.expression.length - 1)
+      });
+    else
+      this.setState({
+        expression: this.state.expression + value
+      });
   }
 
   render() {
     return (
       <div className="app">
         <h1>Sezzle Calculator</h1>
-        <Calculator />
+        <Calculator handleOnClick={this.handleOnClick.bind(this)} />
+        <h2>{this.state.expression}</h2>
       </div>
-    )
+    );
   }
 }
 
--- client/src/components/Calculator.jsx
+++ client/src/components/Calculator.jsx
@@ -3,22 +3,26 @@ import '../css/Calculator.css';
 
 const Calculator = props => (
   <div className="calculator">
-    <div className="button">7</div>
-    <div className="button">8</div>
-    <div className="button">9</div>
-    <div className="button">÷</div>
-    <div className="button">4</div>
-    <div className="button">5</div>
-    <div className="button">6</div>
-    <div className="button">x</div>
-    <div className="button">1</div>
-    <div className="button">2</div>
-    <div className="button">3</div>
-    <div className="button">-</div>
-    <div className="button">0</div>
-    <div className="button">.</div>
-    <div className="button">=</div>
-    <div className="button">+</div>
+    <div onClick={props.handleOnClick} data-value="C" className="button">C</div>
+    <div onClick={props.handleOnClick} data-value="(" className="button">(</div>
+    <div onClick={props.handleOnClick} data-value=")" className="button">)</div>
+    <div onClick={props.handleOnClick} data-value="CE" className="button">CE</div>
+    <div onClick={props.handleOnClick} data-value="7" className="button">7</div>
+    <div onClick={props.handleOnClick} data-value="8" className="button">8</div>
+    <div onClick={props.handleOnClick} data-value="9" className="button">9</div>
+    <div onClick={props.handleOnClick} data-value="÷" className="button">÷</div>
+    <div onClick={props.handleOnClick} data-value="4" className="button">4</div>
+    <div onClick={props.handleOnClick} data-value="5" className="button">5</div>
+    <div onClick={props.handleOnClick} data-value="6" className="button">6</div>
+    <div onClick={props.handleOnClick} data-value="x" className="button">x</div>
+    <div onClick={props.handleOnClick} data-value="1" className="button">1</div>
+    <div onClick={props.handleOnClick} data-value="2" className="button">2</div>
+    <div onClick={props.handleOnClick} data-value="3" className="button">3</div>
+    <div onClick={props.handleOnClick} data-value="-" className="button">-</div>
+    <div onClick={props.handleOnClick} data-value="0" className="button">0</div>
+    <div onClick={props.handleOnClick} data-value="." className="button">.</div>
+    <div  data-value="=" className="button">=</div>
+    <div onClick={props.handleOnClick} data-value="+" className="button">+</div>
   </div>
 );
 
